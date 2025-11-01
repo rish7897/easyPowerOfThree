0f01794a35c8ecc05ad45b604793c1cd5224cc32
Rishabh Chaturvedi <118957285+rish7897@users.noreply.github.com>
2025-11-01 07:49:58 +0530

    second commit

--- method4.cpp
+++ method4.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << 1 << endl;
+// bitwise method
+bool isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+    }
+    int num = 1;
+    while(num < n){
+            num += (num << 1);
+            if(num > INT_MAX / 3){
+                break;
+        }
+    }
+    if(num == n){
+            return true;
+    }
+    return false;
 }
